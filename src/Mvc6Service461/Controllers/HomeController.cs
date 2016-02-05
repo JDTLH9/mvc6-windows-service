1a7f560c00dd9a55cbd5d6ac85bc833f6849829a
Jonathan Bass <jonathan.bass@advancedcomputersoftware.com>
2016-02-05 14:44:50 +0000

    initial commit

---
+++ src/Mvc6Service461/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+using Mvc6Service461.Models;
+
+namespace Mvc6Service461.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new HomeModel();
+            return View(model);
+        }
+    }
+}
